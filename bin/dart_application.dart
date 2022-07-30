bc101181adaee7872c90fa3ff58a402a639dc9d3
leoFlutterNodeJs <96691500+leoFlutterNodeJs@users.noreply.github.com>
2022-07-30 06:59:26 -0300

    Revert "Aula 1: Feature Hello World Olá mundo"

--- bin/dart_application.dart
+++ bin/dart_application.dart
@@ -1,5 +1,3 @@
-import '../lib/dart_application.dart' as dart_application;
-
 void main(List<String> arguments) {
-  print('Olá mundo! - Hello World! ${dart_application.calculate()}!');
+  print('Hello world!');
 }
--- lib/dart_application.dart
+++ lib/dart_application.dart
@@ -1,3 +1,3 @@
 int calculate() {
-  return 5 * 5;
+  return 6 * 7;
 }
