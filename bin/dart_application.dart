0f512b1c5f0d7451ff5f00e56e10a26d538f9be4
leoFlutterNodeJs <leodevnew@gmail.com>
2022-07-30 06:56:05 -0300

    feat: add calculate to project

--- bin/dart_application.dart
+++ bin/dart_application.dart
@@ -1,3 +1,5 @@
+import '../lib/dart_application.dart' as dart_application;
+
 void main(List<String> arguments) {
-  print('Olá mundo! - Hello World!');
+  print('Olá mundo! - Hello World! ${dart_application.calculate()}!');
 }
